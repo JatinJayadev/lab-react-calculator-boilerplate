84be9ec9d570929f9b3f01d97ef1655c6c87d559
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-28 10:32:09 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calculator from './Components/Calculator';
+
+const App = () => {
+
+
+  return (
+    <div className='main-container' >
+      <h1>CALCULATOR-APP</h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
